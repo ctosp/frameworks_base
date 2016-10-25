691f15319af73780b5f772429dcdc8422872ac3d
Carlo Savignano <carlosavignano@aospa.co>
2016-10-25 17:12:49 +0800

    fingerprint: notify client when cancelling succeeded
    * Some fingerprint hardware hals might not notify userspace in time
      in some scenarios when successful authentication cancel is performed.
    * This is observed for example in some devices that have power and/or home
      buttons merged with fingerprint sensor, but not limited to such cases.
    * Add a opt-in configuration to enable client authentication cancel from
      fingerprint service when successful.
    * This addresses originally an issue where requesting fingerprint to be
      disabled and then enabled will wait till cancelling timeout before changing
      fingerprint sensor state and start authenticating again.
    
    Change-Id: I6a6795fbb795f0c6a4ff8ad27ac807e2f744c2d9
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -87,6 +87,7 @@ public abstract class BiometricServiceBase extends SystemService
     private final ResetClientStateRunnable mResetClientState = new ResetClientStateRunnable();
     private final ArrayList<LockoutResetMonitor> mLockoutMonitors = new ArrayList<>();
     private final boolean mCleanupUnusedFingerprints;
+    private final boolean mNotifyClient;
 
     protected final IStatusBarService mStatusBarService;
     protected final Map<Integer, Long> mAuthenticatorIds =
@@ -659,6 +660,8 @@ public abstract class BiometricServiceBase extends SystemService
         mMetricsLogger = new MetricsLogger();
         mCleanupUnusedFingerprints = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_cleanupUnusedFingerprints);
+        mNotifyClient = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_notifyClientOnFingerprintCancelSuccess);
     }
 
     @Override
@@ -841,7 +844,11 @@ public abstract class BiometricServiceBase extends SystemService
             ClientMonitor client = mCurrentClient;
             if (client instanceof EnrollClient && client.getToken() == token) {
                 if (DEBUG) Slog.v(getTag(), "Cancelling enrollment");
-                client.stop(client.getToken() == token);
+                final int stopResult = client.stop(client.getToken() == token);
+                if (mNotifyClient && (stopResult == 0)) {
+                    handleError(mHalDeviceId,
+                            BiometricConstants.BIOMETRIC_ERROR_CANCELED, 0);
+                }
             }
         });
     }
